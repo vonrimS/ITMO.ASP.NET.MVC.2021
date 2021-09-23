4cfe7bc9fc41b3f4f987fc1f73899692db9aaccd
Михаил Смирнов <mikhail.smirnov@MacBook-Air-ekaterina.local>
2021-09-23 20:53:13 +0300

    Lab 01

---
+++ MVC_Lab01/Models/ModelClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MVC_Lab01.Models
+{
+    public class ModelClass
+    {
+        public static string ModelHello()
+        {
+            int hour = DateTime.Now.Hour;
+            string Greeting = hour < 12 ? "Доброе утро" : "Добрый день";
+            return Greeting;
+        }
+    }
+}
