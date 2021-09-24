4ca2ef2c572492748a4311c3b80d58760ffdc181
Михаил Смирнов <mikhail.smirnov@MacBook-Air-ekaterina.local>
2021-09-24 23:54:09 +0300

    Lab 02 Exercise 01, 02

--- MVC_Lab01/Controllers/HomeController.cs
+++ MVC_Lab01/Controllers/HomeController.cs
@@ -14,10 +14,70 @@ namespace MVC_Lab01.Controllers
 
         public string Index(string hel)
         {
-            
-            string Greeting = ModelClass.ModelHello() + ", " + hel;
-            return Greeting;
+
+            //string Greeting = ModelClass.ModelHello() + ", " + hel;
+            //return Greeting;
+
+
+            //string res = ExeEnum();
+            //return res;
+
+            //string res = ExeStruct();
+
+            //string res = StudyCsharp.SetStatus(3);
+
+            //string res = StudyCsharp.ExeSwitch(StudyCsharp.SetStatus(3));
+
+            //string res = StudyCsharp.GetFunction(0, 9);
+
+
+            string res = ExeFactorial(5);
+
+            return res;
         }
+
+        public string ExeEnum()
+        {
+            AccountType goldAccount;
+            AccountType platinumAccount;
+
+            goldAccount = AccountType.Checking;
+            platinumAccount = AccountType.Deposit;
+
+            string res1 = String.Format("Тип банковского чета {0}", goldAccount);
+            string res2 = String.Format("Тип банковского чета {0}", platinumAccount);
+
+            string res = res1 + "\n" + res2;
+            return res;
+        }
+
+        public string ExeStruct()
+        {
+            BankAccount goldBankAccount;
+            goldBankAccount.accType = AccountType.Checking;
+            goldBankAccount.accBal = (decimal)3200.00;
+            goldBankAccount.accNo = 123;
+
+            //string res = String.Format("Номер счета {0}, баланс {1}, тип {2}", goldBankAccount.accNo, goldBankAccount.accBal, goldBankAccount.accType);
+
+            string res = String.Format("Информация о банковском счете: {0}", goldBankAccount);
+
+            return res;
+        }
+
+
+        public string ExeFactorial (int x)
+        {
+            int f;
+            bool ok = StudyCsharp.Factorial(x, out f);
+
+            if (ok)
+                return String.Format("Факториал числа {0} равен {1}", x, f);
+            else
+                return "Невозможно вычислить факториал";
+        }
+
+
         //private readonly ILogger<HomeController> _logger;
 
         //public HomeController(ILogger<HomeController> logger)
---
+++ MVC_Lab01/Models/StudyCsharp.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Text;
+
+namespace MVC_Lab01.Models
+{
+
+    public enum AccountType
+    {
+        Checking,
+        Deposit
+    }
+
+    public struct BankAccount
+    {
+        public long accNo;
+        public decimal accBal;
+        public AccountType accType;
+
+        public override string ToString()
+        {
+            //string res = String.Format("Номер счета {0}, баланс {1}, тип {2}", accNo, accBal, accType);
+            string res = $"Номер счета {accNo}, баланс {accBal}, тип {accType}";
+            return res;
+        }
+    }
+
+    public class StudyCsharp
+    {
+        
+        public static string SetStatus (int age)
+        {
+            string status = "junior developer";
+
+            if ((age > 2) && (age < 7)) status = "middle developer";
+            else if ((age >= 7) && (age < 15)) status = "senior developer";
+            else if ((age >= 15)) status = "sensei";
+
+            return status;
+        }
+
+        public static string ExeSwitch(string status)
+        {
+            string res;
+            switch (status)
+            {
+                case "junior developer":
+                    res = "Набирайся знаний, ешь сметану и творог"; break;
+                case "middle developer":
+                    res = "Набирайся опыта, не забывай про знания"; break;
+                case "senior developer":
+                    res = "Руководи другими (но главное чтобы санитары не заметили)"; break;
+                case "sensei":
+                    res = "Учи других"; break;
+                default:
+                    res = "Не знаю что и думать"; break;
+            }
+            return res;
+        }
+
+        public static string GetFunction (double x1, double x2)
+        {
+            StringBuilder str = new StringBuilder();
+            double x = x1;
+            do
+            {
+                str.AppendFormat("x = {0:0.##} : y = {1:0.##};\n", x, Math.Pow(x, 3));
+                x = x + 0.5;
+            } while (x <= x2);
+
+            return str.ToString();
+        }
+
+        public static bool Factorial (int n, out int answer)
+        {
+            int k;
+            int f = 1;
+            bool ok = true;
+
+            try
+            {
+
+                checked
+                {
+                    for (k = 2; k <= n; ++k)
+                    {
+                        f = f * k;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                f = 0;
+                ok = false;
+            }
+
+            answer = f;
+            return ok;
+        }
+    }
+}
