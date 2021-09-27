8a7ebb4f0c361bc3c54b5a984337e2718ac46163
vonrimS <mr.mytab@gmail.com>
2021-09-27 18:19:09 +0300

    Lab 06

--- MvcCreditApp001/Models/Bid.cs
+++ MvcCreditApp001/Models/Bid.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvcCreditApp001.Models
 {
     public class Bid
     {
         public virtual int BidId { get; set; }
+        [Required]
+        [DisplayName("Имя заявителя")]
         public virtual string Name { get; set; }
+        [Required]
+        [DisplayName("Название кредита")]
         public virtual string CreditHead { get; set; }
+        [DisplayName("Дата подачи заявки")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public virtual DateTime bidDate { get; set; }
     }
 }
--- MvcCreditApp001/Models/Credit.cs
+++ MvcCreditApp001/Models/Credit.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvcCreditApp001.Models
 {
@@ -9,9 +11,17 @@ namespace MvcCreditApp001.Models
     public class Credit
     {
         public virtual int CreditId { get; set; }
+        [Required]
+        [DisplayName("Наименование кредита")]
         public virtual string Head { get; set; }
+        [Required]
+        [DisplayName("Срок кредитования")]
         public virtual int Period { get; set; }
+        [Required]
+        [DisplayName("Тело кредита")]
         public virtual int Sum { get; set; }
+        [Required]
+        [DisplayName("Интерес банка")]
         public virtual int Procent { get; set; }
 
     }
